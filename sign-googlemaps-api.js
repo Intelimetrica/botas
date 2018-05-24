73af0aafc5e8a715dd622d3785f6b7648031e1dd
Roberto Antelo <roberto_antelo@hotmail.com>
2018-05-24 15:42:02 -0500

    Format documentation for googlemaps apis

--- sign-googlemaps-api.js
+++ sign-googlemaps-api.js
@@ -1,3 +1,14 @@
+/**
+ * Signature generating algorithm: (extracted from https://developers.google.com/maps/documentation/maps-static/get-api-key#client-id)
+ *
+ * 1. Generate query with clientID (signature is not added yet)
+ * 2. Strip domain from request ( localhost:3000/  -- /maps/api/static?center=... )
+ * 3. Retrieve private key, originally in Base64
+ *    Sign the URL using HMAC-SHA1. First decode the private key.
+ *    Note that Base64 for URLS replaces + and / with - and _
+ * 4. Encode the resulting binary signature using the modified Base64
+ * 5. Attach the signature to the URL
+ **/
 const crypto = require('crypto');
 const { parse, format } = require('url');
 
@@ -18,17 +29,13 @@ const encodeBase64Hash = (key, data) => crypto
   .update(data)
   .digest('base64');
 
-/* *
- * Signature generating algorithm:
+/**
+ * sign
  *
- * 1. Generate query with clientID (signature is not added yet)
- * 2. Strip domain from request ( localhost:3000/  -- /maps/api/static?center=... )
- * 3. Retrieve private key, originally in Base64
- *    Sign the URL using HMAC-SHA1. First decode the private key.
- *    Note that Base64 for URLS replaces + and / with - and _
- * 4. Encode the resulting binary signature using the modified Base64
- * 5. Attach the signature to the URL
- * */
+ * @param path
+ * @param secret
+ * @returns {undefined}
+ */
 const sign = (path, secret) => {
   const uri = parse(path);
   const webSafeSecret = decodeBase64Hash(unmakeWebSafe(secret));
