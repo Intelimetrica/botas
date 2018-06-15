27cb648f4bc77f625169825b6ae6dfaa8dd7bf29
Roberto Antelo <roberto_antelo@hotmail.com>
2018-06-15 18:45:36 -0500

    Refactor S3 scripts

--- scripts/upload-s3.js
+++ scripts/upload-s3.js
@@ -7,7 +7,7 @@ const AWS = require("aws-sdk");
  * In order to use this method you should have AWS_ACCESS_KEY_ID and
  * AWS_SECRET_ACCESS_KEY env variables set in the process.env
  *
- * @param {string} filename - Filename of Object to be uploaded. Must contain extension
+ * @param {string} filename - Filename of Object to be uploaded. Must contain extension.
  * @param {Binary string} content - File content in a Binary string.
  * @param {string} permission - Object's permission. "private" by default. Available options private | public-read | public-read-write | authenticated-read | aws-exec-read | bucket-owner-read | bucket-owner-full-control
  * @param {string} AWS_region - Bucket's AWS region. https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.RegionsAndAvailabilityZones.html
@@ -18,7 +18,7 @@ const AWS = require("aws-sdk");
 const uploadToS3 = (filename, content, permission="private", AWS_region, AWS_bucket) => {
   if (!content) return Promise.reject("Content cannot be empty");
   if (!AWS_region) return Promise.reject("AWS REGION is required");
-  if (!params.Bucket || !params.Key) return Promise.reject("AWS_bucket and filename cannot be empty");
+  if (!AWS_bucket || !filename) return Promise.reject("AWS_bucket and filename cannot be empty");
 
   let params = {
     Key: filename,
