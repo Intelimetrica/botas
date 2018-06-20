6b52cff70d4955375f9ebf8dc4fc9c1734a3a5bd
Roberto Antelo <roberto_antelo@hotmail.com>
2018-06-20 09:43:09 -0500

    Polish docs of formatters

--- utilities/formatters/index.js
+++ utilities/formatters/index.js
@@ -9,14 +9,34 @@ const { flow, toFixed, isNil } = require("../general");
 const { addPostfix, separate } = require("../string");
 
 /**
- * toPercentage
+ * Separate input from right to left in buckets of 3 digits using commas.
+ * Useful to format prices.
+ *
+ * @example
+ * separateThousands(12000); //=>'12,000'
+ * separateThousands('1250000'); //=> '1,250,000'
+ *
+ * @param {number|string} value - Input that will be separated with commas every 3 elements
+ * @returns {string}
+ */
+const separateThousands = value => {
+  if (isNil(value)) return null;
+  return separate(value, 3, ',', true);
+}
+
+/**
  * Run a pipeline of formatters to transform a ranged 0 to 1 number into a
  * readable percentage.
  * The output would look like `XX.XX %`
  *
- * @param {number} value
- * @param {number} decimal_points
+ * @example
+ * toPercentage(0.67489); //=> '67.49 %'
+ * toPercentage(0.67439, 1); //=> '67.4 %'
+ *
+ * @param {number} value - Floating precision number to transform into percentage
+ * @param {number} decimal_points - Floating points to take in the final format
  * @returns {string}
+ *
  */
 const toPercentage = (value, decimal_points=2) => flow([
   x => x * 100,
@@ -24,20 +44,9 @@ const toPercentage = (value, decimal_points=2) => flow([
   addPostfix(" %")
 ])(value);
 
-/**
- * separateThounsands
- *
- * @param {number|string} value
- * @returns {string}
- */
-const separateThounsands = value => {
-  if (isNil(value)) return null;
-  return separate(value, 3, ',', true);
-}
-
 //TODO: Add price, area, distance and range formatters
 
 module.exports = {
-  separateThounsands,
+  separateThousands,
   toPercentage
 }
