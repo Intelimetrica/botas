b353e1281f18ebdf20ca1051410d088ce596be9c
Ian Gonzalez <igonzalez@intelimetrica.com>
2018-06-25 12:15:16 -0500

    Change download JSON file comments (example and doc generation)

--- scripts/download_json/index.js
+++ scripts/download_json/index.js
@@ -1,22 +1,16 @@
 'use strict'
 
 /**
- * Random scripts
- * @module Scripts
- */
-
-/**
  * Generates the string to use as download link of a object in JSON format
  *
+ * @example
+ * <a href={getJSONDownloadString(object)} download={`${fileName}.json`}>Download JSON</a>
  *
- * @param {object} Object - Object that will be downloaded as JSON
- * Example:
- *  <a href={getJSONDownloadString(object)} download={`${fileName}.json`}>
- *    Download JSON
- *  </a>
+ * @memberof module:Scripts
+ * @param {Object} obj - Object that will be downloaded as JSON
  * @returns {string}
  */
-const getJSONDownloadString = (object) => {
+const getJSONDownloadString = (obj) => {
   const formattedObject = encodeURIComponent(JSON.stringify(object));
   return `data:text/json;charset=utf-8,${formattedObject}`;
 }
